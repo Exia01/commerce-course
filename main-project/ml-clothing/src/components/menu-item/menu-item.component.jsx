28b38e55c980c50f0e1ba474ed98e47ceabd657f
Jose <gonzalezj-@protonmail.com>
2020-10-12 15:53:09 -0400

    finish breaking homepage into smaller components, finish style for menu item component

--- main-project/ml-clothing/src/components/menu-item/menu-item.component.jsx
+++ main-project/ml-clothing/src/components/menu-item/menu-item.component.jsx
@@ -6,12 +6,16 @@ import './menu-item.styles.scss';
 export default function MenuItem({ title, imageUrl, size }) {
   //size if optional, will pass to class if there no need for checking
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      {/* We want image to style within the size of the containing div */}
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>
