55e6347fbbee3db2b2ccfbd5604d68431d8fa207
Jose <gonzalezj-@protonmail.com>
2020-10-15 22:00:40 -0400

    create header component and add to app js

--- main-project/ml-clothing/src/App.js
+++ main-project/ml-clothing/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 // switch will ensure only one renders one route. Gives more control over code
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -9,10 +10,11 @@ import './App.css';
 function App() {
   return (
     <main>
+      <Header />
       <Switch>
         {/* route passes props only one component deep */}
         <Route exact path='/' component={Homepage} />
-        <Route  path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </main>
   );
