9fc9e30202f1b71d02fb8dc416e888b0bccb7ef5
Jose <gonzalezj-@protonmail.com>
2020-10-15 21:33:05 -0400

    Add shop page, create collection preview and collection item component

--- main-project/ml-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ main-project/ml-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 
 // styles
 import './collection-preview.styles.scss';
@@ -10,12 +11,8 @@ export default function CollectionPreview({ title, items }) {
       <div className='preview'>
         {items
           .filter((item, idx) => idx < 4) //only 4 items can tell by index could use slice?
-          .map((item) => {
-            return (
-              <div key={item.id} className=''>
-                {item.name}
-              </div>
-            );
+          .map(({ id, ...otherItemProps }) => {
+            return <CollectionItem key={id} {...otherItemProps} />;
           })}
       </div>
     </div>
