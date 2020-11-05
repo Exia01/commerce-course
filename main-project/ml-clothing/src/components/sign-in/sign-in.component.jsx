6c411455145fcbb06af2bb92b876a56bb2a40b03
Jose <gonzalezj@protonmail.com>
2020-11-04 21:10:56 -0500

    finish sign in handleSubmit method

--- main-project/ml-clothing/src/components/sign-in/sign-in.component.jsx
+++ main-project/ml-clothing/src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -13,17 +13,24 @@ class SignIn extends React.Component {
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password); //if succeeds clear state
+      this.setState({ email: '', password: '' });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     const { value, name } = e.target; //destructuring
     this.setState({ [name]: value });
   };
