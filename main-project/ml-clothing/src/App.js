cbe8c680eaaf6b32a0a653b1e27ec2dd36195b35
Jose <gonzalezj@protonmail.com>
2020-11-24 21:57:43 -0500

    Add checkout component. Add redux selectors to App

--- main-project/ml-clothing/src/App.js
+++ main-project/ml-clothing/src/App.js
@@ -5,12 +5,21 @@ import { connect } from 'react-redux'; //connects to the redux store
 import { setCurrentUser } from './redux/user/user.actions'; //redux action
 
 import './App.css';
+// Components
+import Header from './components/header/header.component';
 
+// Pages
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { selectCurrentUser } from './redux/user/user.selector';
+import { createStructuredSelector } from 'reselect';
 
 class App extends React.Component {
   constructor() {
@@ -56,6 +65,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
@@ -69,9 +79,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  //destructuring slice from the store obj
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  //even though we only have one prop, can make this easier in the future
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- main-project/ml-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ main-project/ml-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = (
+  { cartItems, history } //getting history props from withRouter
+) => (
   //functional component. //stateless
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? ( //falsey or truthy
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className='empty-message'>Your Cart is empty</span>
+      )}
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems, //memoized selector
+});
+
+//all hoc return components and take components ==> stacking HOCS. Order matters!!!
+export default withRouter(connect(mapStateToProps, null)(CartDropdown));
+
+/*could also pass the state like this:
 const mapStateToProps = (state) => ({
   cartItems: selectCartItems(state), //memoized selector
-});
-export default connect(mapStateToProps, null)(CartDropdown);
+}); */
