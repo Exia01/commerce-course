5c87e3b7d1970f8a22955c94a0d7a456671598eb
Jose Gonzalez <gonzalezj@protonmail.com>
2021-02-05 19:18:50 -0500

    update shop data, reducer and collection component

--- main-project/ml-clothing/src/pages/collection/collection.component.jsx
+++ main-project/ml-clothing/src/pages/collection/collection.component.jsx
@@ -5,17 +5,24 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
-
-const CollectionPage = ({ collection }) => (
-  <div className='category'>
-    <h2>Collection Page</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 // ownProps is props from component we're wrapping in
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
-  // passing the state is necessary because unlike other selectors, this selector needs a part of the state that depends on the url??? 
+  // passing the state is necessary because unlike other selectors, this selector needs a part of the state that depends on the url???
   // We are not using a createSelector so that requires us to pass the state
 });
 export default connect(mapStateToProps)(CollectionPage);
