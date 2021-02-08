37b1055336d94c39c28481e5806dd1a1f8ab5af2
Jose <gonzalezj@protonmail.com>
2021-02-08 17:12:19 -0500

    Add styled Homepage component

--- main-project/ml-clothing/src/pages/homepage/homepage.component.jsx
+++ main-project/ml-clothing/src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-
 import Directory from '../../components/directory/directory.component';
 
+//Styled component
+import { HomePageContainer } from './homepage.styles';
+
+// //No longer needed
 import './homepage.styles.scss';
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
---
+++ main-project/ml-clothing/src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div` //it is a component so we use that 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
